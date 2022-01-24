b8091a32d9583a3c57c70243ebb8aadc237eb08e
30sky09 <88087506+30sky09@users.noreply.github.com>
2022-01-24 20:51:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -108,3 +108,43 @@ function my_keydown(e) {
         console.log("c");
     }
 }
+
+function up() {
+    if (player_y >= 0) {
+        player_y = player_y - block_image_height;
+        console.log("block image height = " + block_image_height);
+        console.log("when up arrow key is pressed,x=" + player_x + ", y= " + player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function down() {
+    if (player_y <= 500) {
+        player_y = player_y + block_image_height;
+        console.log("block image height = " + block_image_height);
+        console.log("when down arrow key is pressed,x=" + player_x + ", y= " + player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function left() {
+    if (player_x >= 0) {
+        player_x = player_x - block_image_width;
+        console.log("block image width = " + block_image_width);
+        console.log("when left arrow key is pressed,x=" + player_x + ", y= " + player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function right() {
+    if (player_x <= 888) {
+        player_x = player_x + block_image_width;
+        console.log("block image width = " + block_image_width);
+        console.log("when right arrow key is pressed,x=" + player_x + ", y= " + player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
